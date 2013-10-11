4642ce317574bfa9ff37c3f50da0eb92559e6c7d
Abhi Agarwal <me@abhiagarwal.com>
2013-10-11 12:32:05 -0400

    Adding 4.py

--- 2. Python/4.py
+++ 2. Python/4.py
@@ -1,7 +1,16 @@
 # Conditional Statements
 
-# A conditional statement is a statement that allows you to check for conditions
-# you defined on variables.
+# A conditional statement is a statement that allows you to check 
+# for conditions on data.
+# The resulting outcome is dependent on the statement after the "if":
+# "if" is true or false
+# so
+# if abhis_age > 16:
+# 	he can drive
+# else:
+# 	he cannot drive
+# We mainly concentrate on the statement "abhis_age > 16"
+# where abhis_age is a variable defined
 
 # For example, I have a variable with a number, and I want to test if the number is
 # Greater than 5. What do I do?
@@ -10,3 +19,5 @@ variable = 5
 
 if variable > 5:
 	print "Number is greater than 5!!"
+else:
+	print "Number is NOT greater than 5"
